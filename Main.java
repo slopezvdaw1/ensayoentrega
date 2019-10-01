b34399ab31444e1bd416d75e02ef3739aa6e4e0a
Sara López <slopezvdaw1@gmail.com>
2019-10-01 12:11:32 +0200

    Eliminada println de Main

--- Main.java
+++ Main.java
@@ -14,7 +14,7 @@ public class Main
      */
     public static void main(String[] args){
          System.out.println("Probando BlueJ");
-         System.out.println("xxxxx");
+
          System.out.println("Modificado desde casa");
     }
 }
